7c1d02499ec9a392861edf9d5f75ba101d4922e5
Parth Gandhi <parthgandhi@outlook.com>
2025-02-22 16:11:06 +0530

    query posthog

---
+++ ask_posthog.py
@@ -0,0 +1,63 @@
+import os
+from dataclasses import dataclass
+from typing import Any
+
+import requests
+
+POSTHOG_HOST = "https://us.posthog.com"
+PROJECT_ID = "128743"
+
+
+@dataclass
+class PostHogResults:
+    results: list[list[Any]] | None = None
+    types: list[str] | None = None
+    columns: list[str] | None = None
+
+
+async def _convert_to_posthog_query(user_input: str) -> str:
+    return ""
+
+
+async def _execute_posthog_query(query: str) -> PostHogResults:
+    # https://posthog.com/docs/sql#query-api
+    api_key = os.getenv("POSTHOG_API_KEY")
+    if not api_key:
+        raise ValueError("POSTHOG_API_KEY is not set")
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}",
+    }
+    data = {
+        "query": {
+            "kind": "HogQLQuery",
+            "query": query,
+        }
+    }
+    response = requests.post(
+        f"{POSTHOG_HOST}/api/projects/{PROJECT_ID}/query", headers=headers, json=data
+    )
+
+    response_json = response.json()
+    print(response_json)
+    return PostHogResults(
+        results=response_json["results"],
+        types=response_json["types"],
+        columns=response_json["columns"],
+    )
+
+
+async def _generate_summary(query_results: PostHogResults) -> str:
+    return ""
+
+
+async def ask(user_input: str) -> str:
+    query = await _convert_to_posthog_query(user_input)
+    query = """select toDate(timestamp) as timestamp, count()
+from events
+group by timestamp
+limit 100"""
+    results = await _execute_posthog_query(query)
+    summary = await _generate_summary(results)
+    return summary
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import logging
 
 import uvicorn
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel
+
+import ask_posthog
+
+load_dotenv()
 
 app = FastAPI()
 
@@ -16,9 +21,16 @@ class UserQueryRequest(BaseModel):
 @app.post("/query")
 async def handle_query(request: UserQueryRequest):
     logger.info(f"Received query: {request.user_query}")
-    return {"status": "success", "received_query": request.user_query}
+    response = await ask_posthog.ask(request.user_query)
+    return {"status": "success", "response": response}
 
 
 if __name__ == "__main__":
     logger.info("Starting FastAPI application")
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_config="log_conf.yaml")
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        log_config="log_conf.yaml",
+        reload=True,
+    )
